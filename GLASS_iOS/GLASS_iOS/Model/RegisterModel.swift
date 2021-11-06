7207a79e4634e3bbaec044348d59487164f872fb
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-11-06 23:03:32 +0900

    Fix: FixCodable
    Codable 코드를 수정하였습니다.

--- GLASS_iOS/GLASS_iOS/Model/RegisterModel.swift
+++ GLASS_iOS/GLASS_iOS/Model/RegisterModel.swift
@@ -34,36 +34,65 @@ struct LogIn: Codable {
 
 struct ImagePost: Codable{
     var status: Int
-    var error: String?
     var message: String?
-    var jsonUrl: String?
+    var jsonUrl: [String]
 }
 
 struct Post: Codable{
     var status: Int
     var error: String?
     var message: String?
+    var jsonUrl: [String]
 }
 
-//class Owner: Codable{
-//    var name: String
-//    var avatar: String
-//    var stuNumber: Int
-//    var classNumber: Int
-//    var grade: Int
-//    var permission: Int
-//}
-//
-//class Writings: Codable{
-//    var hashtags: String?
-//    var imgs: String?
-//    var text: String?
-//    var _id: String?
-//    var owner: Owner
-//    var isLike: DarwinBoolean
-//    var likeCount: Int
-//}
-//class HomeResponse: Codable{
-//    var writings: Writings
-//}
-//
+class Owner: Codable {
+    var name: String
+    var avatar: String
+    var stuNumber: Int
+    var classNumber: Int
+    var grade: Int
+    var permission: Int
+    
+    enum OwnerKeys: String, CodingKey {
+        case name
+        case avatar
+        case stuNumber
+        case classNumber
+        case grade
+        case permission
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: OwnerKeys.self)
+        
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+        self.avatar = try container.decodeIfPresent(String.self, forKey: .avatar) ?? ""
+        self.stuNumber = try container.decodeIfPresent(Int.self, forKey: .stuNumber) ?? 0
+        self.classNumber = try container.decodeIfPresent(Int.self, forKey: .classNumber) ?? 0
+        self.grade = try container.decodeIfPresent(Int.self, forKey: .grade) ?? 0
+        self.permission = try container.decodeIfPresent(Int.self, forKey: .permission) ?? 0
+    }
+    
+    internal init(name: String, avatar: String, stuNumber: Int, classNumber: Int, grade: Int, permission: Int) {
+        self.name = name
+        self.avatar = avatar
+        self.stuNumber = stuNumber
+        self.classNumber = classNumber
+        self.grade = grade
+        self.permission = permission
+    }
+}
+
+struct Writings: Codable{
+    var hashtags: [String?]
+    var imgs: [String]
+    var text: String?
+    var _id: String
+    var owner: Owner
+    var isLike: Bool
+    var likeCount: Int
+}
+
+struct HomeResponse: Codable{
+    var writings: [Writings]
+}
